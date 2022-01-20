a0ecb40359b26e5bb50ab523541caa7c8b8d2d3c
elexis <eebrown422@gmail.com>
2022-01-19 23:08:12 -0500

    the

---
+++ app.js
@@ -0,0 +1,33 @@
+//Step 1 - Declare and Assign Variables
+var a = 1; 
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+var f = 6;
+var g = 7;
+var h = 8;
+var i =9;
+var j = 10;
+//Step 2 - Addition
+console.log(a + b);
+console.log(c + d);
+//Step 3 - Subtraction
+console.log(e - f);
+console.log(g - h);
+//Step 4 - Multiplication
+console.log(i * j);
+console.log(a * b);
+//Step 5 - Division
+console.log(f/c);
+console.log(j/e);
+//Step 6 - Incrementation
+a++;
+c++;
+console.log(a);
+console.log(c);
+//Step 7 - Decrementation
+a--;
+c--;
+console.log(a);
+console.log(c);
